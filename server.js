e1cf6a415214405cd972a1d7ff187af5a1a45572
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-02-17 18:42:23 +0900

    값을 꺼내서 뿌려주는 table 만들어주는것 까지 성공

--- server.js
+++ server.js
@@ -9,12 +9,52 @@ var finalhandler = require('finalhandler');
 
 var router = Router();
 
+
+var mock_data = { homekeepers : [
+    {
+        id : 1,
+        payDate : "2016-02-11",
+        inOut : "in",
+        content : "월급",
+        money : 1700000
+    },
+    {
+        id : 2,
+        payDate : "2016-02-11",
+        inOut : "in",
+        content : "책",
+        money : 15000
+    },
+    {
+        id : 3,
+        payDate : "2016-02-11",
+        inOut : "in",
+        content : "피자 소주 (아빠 생일)",
+        money : 20000
+    },
+    {
+        id : 4,
+        payDate : "2016-02-16",
+        inOut : "out",
+        content : "가방",
+        money : 20000
+    },
+]};
+
+function errhandler(err){
+    console.log("에러 처리 : ");
+    console.log(err);
+}
+
+function extractFileExt(filePath){
+    return path.extname(filePath).substring(1);
+}
+
+
 router.get('/public/*', function (req, res) {
-	//console.log(req.path);
+
 	var filePath = '.' + url.parse(req.url).pathname;
-	//console.log(filePath);
-    //파일확장자 추출
-	var ext = path.extname(filePath).substring(1);
+	var ext = extractFileExt(filePath);
 
     fs.access(filePath, fs.R_OK, function(err){
 
@@ -24,11 +64,12 @@ router.get('/public/*', function (req, res) {
             rs.pipe(res);
             res.end();
         } else {   //파일이 있는 경우
-            var rs = fs.createReadStream(filePath); //rederStream
-            rs.on('error', function(err){ // 에러 처리
-                console.log("에러 처리 : ");
-                console.log(err);
-            });//rs.on(err)
+            //err handler
+
+
+            var rs = fs.createReadStream(filePath);
+            rs.on('error', errhandler);
+
             var contextType = '';
             //확장자로 컨텍스트 타입 설정
             switch(ext) {
@@ -68,18 +109,32 @@ router.get('/moneyplaner', function (req, res)  {
 
             var rs = fs.createReadStream(filePath); //rederStream
             rs.on('error', function(err){ // 에러 처리
-                console.log("에러 처리 : ");
-                console.log(err);
+                console.log("에러 처리 : "); console.log(err);
             });//rs.on(err)
 
-            //content-type을 세팅해줌
-            res.setHeader("Content-Type", "text/html");
 
-            rs.pipe(res);//responce 파이핑 해 파일 내보내기
+            res.setHeader("Content-Type", "text/html");
+            rs.pipe(res);
+
         }// if(err)
 
     });//fs.access
 
+});
+
+router.get('/api/homekeepers', function (req, res)  {
+
+    var body = [];
+   /* req.on('data', function(chunk) {
+        console.log("chunk : " + chunk);
+        body.push(chunk);
+    }).on('end', function() {
+        console.log(body);
+    });*/
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(mock_data));
+    console.log(JSON.stringify(mock_data));
+    res.end();
 
 });
 
